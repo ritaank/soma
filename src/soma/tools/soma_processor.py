ritaank/soma

commit 4ff45cfbaff45c07b52bce129a43bb3444e95030

Update args for write_mocap_c3d function

src/soma/tools/soma_processor.py
@@ -378,7 +378,7 @@ class SOMAMoCapPointCloudLabeler(object):
         # ToDo: if a point label is inside a tracklet with the same label keep the confidence if not plunge it to zero
 
         labeling_results = self.create_mocap(points=self.points_compressed,
-                                             points_label_id=points_label_id_tracked,
+                                             points_label_id=points_label_id_tracked,ody_visualizer
                                              superset_labels=self.superset_labels,
                                              keep_nan_points=self.rt_cfg.keep_nan_points,
                                              remove_zero_trajectories=self.rt_cfg.remove_zero_trajectories, )
@@ -448,7 +448,7 @@ def run_soma_once(soma_runtime_cfg):
     if rt_cfg.save_c3d:
         c3d_out_fname = rt_cfg.dirs.mocap_out_fname.replace('.pkl', '.c3d')
         nan_replaced_labels = [l if l != 'nan' else '*{}'.format(i) for i, l in enumerate(results['labels'])]
-        write_mocap_c3d(out_c3d_fname=c3d_out_fname,
+        write_mocap_c3d(out_mocap_fname=c3d_out_fname,
                         markers=results['markers'],
                         labels=nan_replaced_labels,
                         frame_rate=soma_labeler.mocap_frame_rate)

commit 364135c09552690468af9bea46b200665b7a5168

fixed typo introduced in 4ff45cf

src/soma/tools/soma_processor.py
@@ -378,7 +378,7 @@ class SOMAMoCapPointCloudLabeler(object):
         # ToDo: if a point label is inside a tracklet with the same label keep the confidence if not plunge it to zero
 
         labeling_results = self.create_mocap(points=self.points_compressed,
-                                             points_label_id=points_label_id_tracked,ody_visualizer
+                                             points_label_id=points_label_id_tracked,
                                              superset_labels=self.superset_labels,
                                              keep_nan_points=self.rt_cfg.keep_nan_points,
                                              remove_zero_trajectories=self.rt_cfg.remove_zero_trajectories, )